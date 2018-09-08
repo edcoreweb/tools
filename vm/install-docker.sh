c1ecc556595d4d64035341672c33603346c14771
edcoreweb <edcoreweb@gmail.com>
2018-09-08 15:52:42 +0300

    Update install-docker.sh

--- vm/install-docker.sh
+++ vm/install-docker.sh
@@ -7,3 +7,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y -q update
 apt-cache policy docker-ce
 
 sudo apt-get install -y docker-ce
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
