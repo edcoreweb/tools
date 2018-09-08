864ed44b1b2751647440e2369d7607097b91868f
edcoreweb <edcoreweb@gmail.com>
2018-09-08 16:14:44 +0300

    Update install-docker.sh

--- vm/install-docker.sh
+++ vm/install-docker.sh
@@ -8,6 +8,9 @@ apt-cache policy docker-ce
 
 sudo apt-get install -y docker-ce
 
+# non-sudo permissions
+sudo usermod -aG docker $USER
+
 # Install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
