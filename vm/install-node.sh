310fa490f31d4410fc424c4a6174151dc8a53ad0
edcoreweb <edcoreweb@gmail.com>
2018-09-13 00:00:33 +0300

    Update install-node.sh

--- vm/install-node.sh
+++ vm/install-node.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
 sudo apt-get install -y nodejs
 
 # Install Yarn
-sudo curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -q update
