84cf03f5edb519fd89948c5f777ba6ea53a307f7
Eduard Tofan <edcoreweb@gmail.com>
2018-09-16 19:33:29 +0300

    Huge speed improvements, php version switcher, xdebug proxy pass, mysql speed improvements, nginx default config

--- vm/install-nginx.sh
+++ vm/install-nginx.sh
@@ -1,209 +1,46 @@
 #!/usr/bin/env bash
 
-DOMAIN1="default.local.ro"
-#DOMAIN2 would be the www variant - not needed here
-DOMAIN2=
+WEB_ROOT="/var/www"
+PHP_VERSIONS=( "5.6" "7.1" "7.2" )
 
-HOME="/var/www"
-#creates HOME/www as web root with www/public_html as public web location
-
-#nginx main location block select - default does not url re-write to index.php
-#default-main-block.conf
-#wp-main-block.conf
-MAIN_BLOCK=default-main-block.conf
-
-php_versions=( "5.6" "7.1" "7.2" )
-
-#install nginx
+# Install nginx
 sudo add-apt-repository ppa:nginx/development -y
 sudo apt-get install nginx -y
 
-#configure nginx (basic)
-sudo sed -i -r \
--e 's/^[ \t]*user www-data;/user www-data;/' \
--e 's/^([ \t]*worker_connections ).*/\1 1024;/' \
--e '/multi_accept/ c \\tmulti_accept on;' \
--e '/keepalive_timeout/ c \\tkeepalive_timeout 15;' \
--e '/server_tokens/ c \\tserver_tokens off;' \
--e '/server_tokens/ a \\tclient_max_body_size 64m;' \
--e '/gzip_proxied/ c \\tgzip_proxied any;' \
--e '/gzip_comp_level/ c \\tgzip_comp_level 2;' \
--e 's/#[ \t]*gzip_types/gzip_types/' \
--e '/default_type/ a \\tadd_header X-Frame-Options SAMEORIGIN;' \
--e '/default_type/ a \\tclient_max_body_size 64M;' \
-/etc/nginx/nginx.conf
+# Copy default configuration files
+[ ! -d "/etc/nginx/custom" ] && \
+sudo mkdir /etc/nginx/custom
 
-sudo service nginx restart
+sudo cp ./config/nginx/index.conf /etc/nginx/conf.d
+sudo cp ./config/nginx/xdebug.conf /etc/nginx/conf.d
+sudo cp ./config/nginx/default-ssl.conf /etc/nginx/custom
+sudo cp ./config/nginx/default-static.conf /etc/nginx/custom
+sudo cp ./config/nginx/default-php.conf /etc/nginx/custom
+sudo cp ./config/nginx/detect-php.conf /etc/nginx/custom
+sudo cp ./config/nginx/default-security.conf /etc/nginx/custom
 
-#remove default site
-sudo rm /etc/nginx/sites-enabled/default > /dev/null 2>&1
-
-#add new default after all others as "not available"
-sudo sed -i -r \
--e '/include \/etc\/nginx\/sites-enabled\/\*;/ a\
-    #default server block\
-    server{\
-        listen 80 default_server;\
-        server_name _;\
-        return 444;\
-    }' \
-/etc/nginx/nginx.conf
-
-#add additional SSL config (will currently get an A in the tests)
-sudo sed -i -r \
--e '/ssl_prefer_server_ciphers on;/ a\
-        ssl_ciphers  HIGH:!aNULL:!MD5;\
-        ssl_session_cache shared:SSL:10m;\
-        ssl_session_timeout 10m;\
-        #add_header Strict-Transport-Security "max-age=31536000; includeSubdomains";\
-    ' \
-/etc/nginx/nginx.conf
-
-sudo service nginx restart
-
-#make web root
-[ ! -d "${HOME}/vhosts" ] && \
-sudo mkdir "${HOME}/vhosts"
-
-#ensure web root is under correct ownership (if running in vagrant with mapped public_html this can get created as root)
-sudo chown $USER:$USER "${HOME}/vhosts"
-sudo chmod 0755 "${HOME}/vhosts"
-
-#set up log directory and public html root
-[ ! -d "${HOME}/vhosts/default" ] && \
-sudo mkdir "${HOME}"/vhosts/default
-
-[ ! -d "${HOME}/logs" ] && \
-sudo mkdir "${HOME}"/logs
-
-#make directory for Lets Encrypt (used later)
-[ ! -d "${HOME}/.well-known/acme-challenge" ] && \
-sudo mkdir -p "${HOME}"/.well-known/acme-challenge
-
-#make sure web directory and file permissions are consistent
-sudo find "${HOME}"/vhosts/ -type d -not -perm 0755 -exec chmod 0755 '{}' \;
-sudo find "${HOME}"/vhosts/ -type f -not -perm 0644 -exec chmod 0644 '{}' \;
-
-#make directory for extra configs to load from nginx server blocks
-[ ! -d "/etc/nginx/sites-available/configs" ] && \
-sudo mkdir /etc/nginx/sites-available/configs
-
-#make self-signed dummy certs
-sudo openssl req -subj "/O=TEST-DEV/C=UK/CN=${DOMAIN1}" -new -newkey rsa:2048 \
--days 365 -nodes -x509 -keyout /etc/nginx/server.key -out /etc/nginx/server.crt > /dev/null
-sudo chmod 0600 /etc/nginx/server.key
-
-#create main server config
-cat << EOF | sudo tee /etc/nginx/sites-available/${DOMAIN1} > /dev/null
-server {
-    listen 80;
-    listen 443 ssl;
-
-    #temporary certs for testing
-    ssl_certificate "/etc/nginx/server.crt";
-    ssl_certificate_key "/etc/nginx/server.key";
-
-    server_name ${DOMAIN1} ${DOMAIN2};
-
-    access_log /var/www/logs/access.log;
-    error_log /var/www/logs/error.log;
-
-    root /var/www/vhosts/default/;
-    index index.php;
-
-    #pull in other locations and configs (eg. wordpress config)
-    include sites-available/configs/extra-config.conf;
-
-    #alias acme dir for Lets Encrypt (used later)
-    location /.well-known {
-        alias "/var/www/.well-known";
-        #index for testing only
-        index index.html;
-
-        allow all;
-    }
-	#main block
-    include sites-available/configs/main-location-block.conf;
-
-    #main php (linked to php.conf unless caching added)
-    include sites-available/configs/php-main.conf;
-}
-EOF
-
-
-cat << EOF | sudo tee /etc/nginx/sites-available/localhost > /dev/null
-#localhost server for ssh tunneled connections
-server {
-    listen 80;
-
-    server_name localhost 127.0.0.1;
-
-    access_log /var/www/logs/access.log;
-    error_log /var/www/logs/error.log;
-
-    root /var/www/vhosts/default/;
-    index index.php;
-
-    #pull in other locations and configs
-    include sites-available/configs/local-extra-config.conf;
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-
-    include sites-available/configs/php7.2.conf;
-}
-
-EOF
-
-#generic php config
-for version in "${php_versions[@]}"
+# Create the upstreams
+for version in "${PHP_VERSIONS[@]}"
 do
-cat << EOF | sudo tee /etc/nginx/sites-available/configs/php${version}.conf > /dev/null
-location ~ \.php\$ {
-    try_files \$uri =404;
-    #fastcgi_split_path_info ^(.+\.php)(/.+)\$;
-    fastcgi_pass unix:/run/php/php${version}-fpm.sock;
-    fastcgi_index index.php;
-    fastcgi_param SCRIPT_FILENAME \$request_filename;
-    include fastcgi_params;
-}
-EOF
+    sudo sed "s/{version}/${version}/g" ./config/nginx/upstream.stub >> /etc/nginx/conf.d/upstream.conf
+    sudo sed -i -e '$a\' /etc/nginx/conf.d/upstream.conf
 done
 
-#main location blocks
-cat << EOF | sudo tee /etc/nginx/sites-available/configs/default-main-block.conf > /dev/null
-#standard main nginx location block
-location / {
-        try_files \$uri \$uri/ =404;
-}
-EOF
+# Remove default site and create new one
+sudo rm /etc/nginx/sites-enabled/default
+sudo cp ./config/nginx/default.conf /etc/nginx/sites-available/default
 
-cat << EOF | sudo tee /etc/nginx/sites-available/configs/wp-main-block.conf > /dev/null
-#wp location block
-location / {
-        try_files \$uri \$uri/ /index.php?\$args;
-}
-EOF
+# Make web root
+[ ! -d "${WEB_ROOT}/vhosts" ] && \
+sudo mkdir "${WEB_ROOT}/vhosts"
 
-#link standard location block
-sudo ln -s /etc/nginx/sites-available/configs/${MAIN_BLOCK} \
-/etc/nginx/sites-available/configs/main-location-block.conf > /dev/null 2>&1
+# Ensure web root is under correct ownership
+sudo chown "{$USER}":"{$USER}" "${WEB_ROOT}/vhosts"
+sudo chmod 0755 "${WEB_ROOT}/vhosts"
 
-#link standard php
-sudo ln -s /etc/nginx/sites-available/configs/php7.2.conf \
-/etc/nginx/sites-available/configs/php-main.conf > /dev/null 2>&1
+# Enable default config
+[ ! -f "/etc/nginx/sites-enabled/default" ] && \
+sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
-#add empty placemarker config file (would get filled in with wp extra config)
-cat << EOF | sudo tee /etc/nginx/sites-available/configs/extra-config.conf > /dev/null
-EOF
-
-#add empty placemarker config file for localhost
-cat << EOF | sudo tee /etc/nginx/sites-available/configs/local-extra-config.conf > /dev/null
-EOF
-
-#enable domain configs
-sudo ln -s /etc/nginx/sites-available/${DOMAIN1} /etc/nginx/sites-enabled/${DOMAIN1} > /dev/null 2>&1
-sudo ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/localhost > /dev/null 2>&1
-
+# Restart
 sudo service nginx restart
--- vm/install-php.sh
+++ vm/install-php.sh
@@ -1,28 +1,18 @@
 #!/usr/bin/env bash
 
-#php_configs=( pm.max_children pm.start_servers pm.min_spare_servers pm.max_spare_servers )
-#micro
-php_configs=( 5 1 1 3 )
-#small
-#php_configs=( 10 2 2 5 )
-#medium
-#php_configs=( 25 4 4 10 )
-#large
-#php_configs=( 50 5 5 30 )
-#xlarge
-#php_configs=( 125 6 6 50 )
-#2xlarge
-#php_configs=( 250 7 7 100 )
+# All available options
+# PHP_VERSIONS: "5.6" "7.0" "7.1" "7.2"
+# PHP_LIB_FOLDERS: "20131226" "20151012" "20160303" "20170718"
 
-php_versions=( "5.6" "7.1" "7.2" )
-php_ini_versions=( "fpm" "cli" )
+PHP_VERSIONS=( "5.6" "7.1" "7.2" )
+PHP_LIB_FOLDERS=( "20131226" "20160303" "20170718" )
 
-#install php
+# Add php repos
 sudo add-apt-repository ppa:ondrej/php -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -q update
 
 # Install PHP
-for version in "${php_versions[@]}"
+for version in "${PHP_VERSIONS[@]}"
 do
     # Install php versions
     sudo apt-get -y install php${version} php${version}-common \
@@ -32,7 +22,7 @@ do
         php${version}-zip php${version}-xml php${version}-opcache php${version}-soap
 
     # Install mcrypt only for 5.6 and 7.1
-    if [ $version = "5.6" ] || [ $version = "7.1" ]
+    if [ $version = "5.6" ] || [ $version = "7.0" ] || [ $version = "7.1" ]
     then
         sudo apt-get -y install php${version}-mcrypt
     fi
@@ -42,66 +32,44 @@ done
 sudo apt-get -y install memcached
 sudo apt-get -y install php-memcached php-mongodb php-memcache php-imagick php-xdebug php-redis
 
-#bit more ram
+# Bit more ram
 sudo sed -i -r 's/^-m[ \t]+64[ \t]*$/-m 96/' /etc/memcached.conf
 
-for version in "${php_versions[@]}"
-do
-    #restart
-    sudo service php${version}-fpm restart
-done
-
-for version in "${php_versions[@]}"
-do
-    #configure fpm php daemon to use the main server user
-    #and set some params (micro bitnami size)
-    sudo sed -i -r \
-    -e '/^user =/ c user = www-data' \
-    -e '/^group =/ c group = www-data' \
-    -e '/^listen\.owner =/ c listen.owner = www-data' \
-    -e '/^listen\.group =/ c listen.group = www-data' \
-    -e '/^pm.max_children =/ c pm.max_children = '${php_configs[0]} \
-    -e '/^pm.start_servers =/ c pm.start_servers = '${php_configs[1]} \
-    -e '/^pm.min_spare_servers =/ c pm.min_spare_servers = '${php_configs[2]} \
-    -e '/^pm.max_spare_servers =/ c pm.max_spare_servers = '${php_configs[3]} \
-    -e '/^;?pm.max_requests =/ c pm.max_requests = 5000' \
-    /etc/php/${version}/fpm/pool.d/www.conf
-done
-
-#get certs for curl
+# Get certs for curl
 sudo curl -s -o /etc/php/cacert.pem  https://curl.haxx.se/ca/cacert.pem
 
-for version in "${php_versions[@]}"
+# Disable xdebug for all versions
+sudo phpdismod -v ALL -s ALL xdebug
+
+# Configure fpm php daemon with two pools w\ xdebug
+for version in "${PHP_VERSIONS[@]}"
 do
-    #configure php defaults
-    for file in "${php_ini_versions[@]}"
-    do 
-        #configure php defaults
-        sudo sed -i -r \
-        -e '/^max_execution_time =/ c max_execution_time = 60' \
-        -e '/^post_max_size =/ c post_max_size = 64M' \
-        -e '/^;cgi.fix_pathinfo=/ c cgi.fix_pathinfo=0' \
-        -e '/^upload_max_filesize =/ c upload_max_filesize = 64M' \
-        -e '/^;date.timezone =/ c date.timezone = "UTC"' \
-        -e '/^session.use_strict_mode =/ c session.use_strict_mode = 1' \
-        -e '/^session.gc_probability =/ c session.gc_probability = 100' \
-        -e '/^session.gc_maxlifetime =/ c session.gc_maxlifetime = 14400' \
-        -e '/^;curl.cainfo =/ c curl.cainfo = /etc/php/cacert.pem' \
-        -e '/^;sendmail_path =/ c sendmail_path = /usr/local/bin/mhsendmail' \
-        /etc/php/${version}/${file}/php.ini
-    done
+    sudo rm /etc/php/${version}/fpm/pool.d/www.conf
+    sudo cp ./config/php/www.conf /etc/php/${version}/fpm/pool.d
+    sudo sed "s/{version}/${version}/g" ./config/php/php-fpm-xdebug.stub > /etc/php/${version}/fpm/php-fpm-xdebug.conf
+    sudo sed "s/{version}/${version}/g" ./config/php/php-fpm-xdebug-service.stub > /lib/systemd/system/${version}-fpm-xdebug.service
 done
 
-#ensure session dir owned by same user as php
+# Install custom config
+for version in "${PHP_VERSIONS[@]}"
+do
+    cp ./config/php/custom.ini /etc/php/${version}/mods-available
+done
+
+# Enable custom config
+phpenmod -v ALL -s ALL custom
+
+# Ensure session dir owned by same user as php
 sudo chown -R www-data:www-data /var/lib/php/sessions
 sudo chmod -R 0755 /var/lib/php/sessions/
 
-for version in "${php_versions[@]}"
+# Restart services
+for version in "${PHP_VERSIONS[@]}"
 do
-    #restart
     sudo service php${version}-fpm restart
+    sudo service php${version}-fpm-xdebug restart
 done
 
-# install Composer
+# Install Composer
 curl -s https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
