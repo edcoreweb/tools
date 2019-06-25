76f372b003ed35c560ad93dd0e09420f297a1e19
edcoreweb <edcoreweb@gmail.com>
2019-06-25 13:23:43 +0300

    Kibana setup.

--- vm/install-elasticsearch.sh
+++ vm/install-elasticsearch.sh
@@ -12,20 +12,23 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -q update
 sudo apt-get -y install elasticsearch
-sudo apt-get -y install kibana
-
-# Start Elasticsearch on boot
-sudo update-rc.d elasticsearch defaults 95 10
-
-# Start Kibana on boot
-sudo update-rc.d kibana defaults 95 10
 
 # Update configuration to use 'local' as the cluster
 sudo sed -i "s/#cluster.name: my-application/cluster.name: local/" /etc/elasticsearch/elasticsearch.yml
 
+# Start Elasticsearch on boot
+sudo update-rc.d elasticsearch defaults 95 10
+
 # Enable Start Elasticsearch
 sudo systemctl enable elasticsearch.service
 sudo service elasticsearch start
+
+# Install Kibana
+sudo apt-get -y install kibana
+sudo sed -i 's/#server.host: "localhost"/server.host: "0.0.0.0"/' /etc/kibana/kibana.yml
+
+# Start Kibana on boot
+#sudo update-rc.d kibana defaults 95 10
 
 # Enable Start Kibana
 sudo systemctl enable kibana.service
