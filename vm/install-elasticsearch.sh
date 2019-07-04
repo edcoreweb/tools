b4e6ca21298a6c3e70bc66698c8991388046d73b
edcoreweb <edcoreweb@gmail.com>
2019-07-04 23:40:36 +0300

    Update java install

--- vm/install-elasticsearch.sh
+++ vm/install-elasticsearch.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
 # Install Java 8
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -q update
-echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
-sudo apt-get -y install oracle-java8-installer
+sudo apt install openjdk-8-jdk
 
 # Install Elasticsearch
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
