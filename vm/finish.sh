a2d60271b5278f71bd4b001b47437651f8f5e7b0
edcoreweb <edcoreweb@gmail.com>
2021-04-06 15:36:38 +0300

    Update finish.sh

--- vm/finish.sh
+++ vm/finish.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-#enable firewall
-#sudo ufw default deny incoming
-#sudo ufw allow ssh
-#sudo ufw allow http
-#sudo ufw allow https
-#echo y | sudo ufw enable
+# Mount from NFS
+cat <<EOF >> .nanorc
+_win_host:/vhosts /var/www/vhosts nfs rw,rsize=32768,wsize=32768,async,timeo=5,nolock,vers=3,noatime,bg
+EOF
