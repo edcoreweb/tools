ec5f98c04dbbdbdbbc46ba4dbb3ea057278f2e11
edcoreweb <edcoreweb@gmail.com>
2021-04-06 15:37:59 +0300

    Update finish.sh

--- vm/finish.sh
+++ vm/finish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Mount from NFS
-cat <<EOF >> .nanorc
+cat <<EOF >> /etc/fstab
 _win_host:/vhosts /var/www/vhosts nfs rw,rsize=32768,wsize=32768,async,timeo=5,nolock,vers=3,noatime,bg
 EOF
