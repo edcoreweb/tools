9a1b301da6243c0c766eeea3496b2ca254bc010c
edcoreweb <edcoreweb@gmail.com>
2021-09-29 21:22:02 +0300

    Update install-docker.sh

--- vm/install-docker.sh
+++ vm/install-docker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -q update
 apt-cache policy docker-ce
@@ -18,5 +18,5 @@ sudo chown fps:fps /home/fps/.docker -R
 sudo chmod g+rwx /home/fps/.docker -R
 
 # Install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
