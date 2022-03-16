6826bb49a6de40b7046f335ab591463d3c3bb27d
edcoreweb <edcoreweb@gmail.com>
2022-03-16 15:01:09 +0200

    Update install-mysql.sh

--- vm/install-mysql.sh
+++ vm/install-mysql.sh
@@ -2,18 +2,28 @@
 
 dir="$(dirname $0)"
 
-MYSQL_ROOT_PASSWORD=""
+# Download
+MYSQL_8="$(wget -qO- https://dev.mysql.com/downloads/repo/apt/ | grep -oP 'mysql-apt-config[^)]+(?=\))' | head -n 1)"
+wget -c "https://dev.mysql.com/get/${MYSQL_8}"
 
-# Install MySQL 5.7
-echo "mysql-server-5.7 mysql-server/root_password password $MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
-echo "mysql-server-5.7 mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD" | sudo debconf-set-selections
-sudo apt-get install -y mysql-server
+# Set config
+MYSQL_ROOT_PASSWORD=""
+sudo debconf-set-selections <<EOF
+mysql-apt-config mysql-apt-config/select-server select mysql-8.0
+mysql-community-server mysql-community-server/root-pass password ${MYSQL_ROOT_PASSWORD}
+mysql-community-server mysql-community-server/re-root-pass password ${MYSQL_ROOT_PASSWORD}
+EOF
+
+# Install MySQL
+export DEBIAN_FRONTEND=noninteractive
+sudo -E dpkg -i ${MYSQL_8}
+sudo apt update
+sudo -E apt install -y mysql-server
 
 # Cleanup
 sudo mysql -u root <<EOF
-CREATE USER 'root'@'%' IDENTIFIED BY '';
+CREATE USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '${MYSQL_ROOT_PASSWORD}';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user = 'root';
 DELETE FROM mysql.user WHERE user = 'root' AND host NOT IN ('%');
 DROP DATABASE IF EXISTS test;
 FLUSH PRIVILEGES;
