681331a078e9310e50c8ee8bfbc530ee18ddd617
edcoreweb <edcoreweb@gmail.com>
2022-03-16 14:02:36 +0200

    Update finish.sh

--- vm/finish.sh
+++ vm/finish.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 # Mount from NFS
-cat <<EOF >> /etc/fstab
+sudo cat <<EOF >> /etc/fstab
 _win_host:/vhosts /var/www/vhosts nfs rw,rsize=32768,wsize=32768,async,timeo=5,nolock,vers=3,noatime,bg
 EOF
+
+# Uninstall cloud init
+sudo apt purge cloud-init -y
+sudo rm -rf /etc/cloud && sudo rm -rf /var/lib/cloud/
