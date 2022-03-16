b0fc8986170ace150193fecbbc6191185c1ba841
edcoreweb <edcoreweb@gmail.com>
2022-03-16 15:41:23 +0200

    Update install-elasticsearch.sh

--- vm/install-elasticsearch.sh
+++ vm/install-elasticsearch.sh
@@ -12,20 +12,14 @@ sudo apt-get -y install elasticsearch
 # Update configuration to use 'local' as the cluster
 sudo sed -i "s/#cluster.name: my-application/cluster.name: local/" /etc/elasticsearch/elasticsearch.yml
 
-# Start Elasticsearch on boot
-#sudo update-rc.d elasticsearch defaults 95 10
-
 # Enable Start Elasticsearch
-sudo systemctl enable elasticsearch.service
-sudo service elasticsearch start
+#sudo systemctl enable elasticsearch.service
+#sudo service elasticsearch start
 
 # Install Kibana
 sudo apt-get -y install kibana
 sudo sed -i 's/#server.host: "localhost"/server.host: "0.0.0.0"/' /etc/kibana/kibana.yml
 
-# Start Kibana on boot
-#sudo update-rc.d kibana defaults 95 10
-
 # Enable Start Kibana
-sudo systemctl enable kibana.service
-sudo service kibana start
+#sudo systemctl enable kibana.service
+#sudo service kibana start
