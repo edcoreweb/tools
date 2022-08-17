549434f7debe038e9184dc85dec3b6660d1b0b41
edcoreweb <edcoreweb@gmail.com>
2022-08-17 15:33:08 +0300

    Update install-elasticsearch.sh

--- vm/install-elasticsearch.sh
+++ vm/install-elasticsearch.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-# Install Java 8
-sudo apt-get -y install openjdk-8-jdk
-
 # Install Elasticsearch
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -q update
 sudo apt-get -y install elasticsearch
