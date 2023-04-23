e91c4aa65439343ed69b66731fb2a9ec2880e9f6
liviuc5psolutions <81562630+liviuc5psolutions@users.noreply.github.com>
2023-04-23 20:42:05 +0300

    Add support for php 8.2 (#2)
    Co-authored-by: CojocaruIonutLiviu <liviu.cojocaru@synergogroup.net>

--- vm/install-nginx.sh
+++ vm/install-nginx.sh
@@ -3,7 +3,7 @@
 dir="$(dirname $0)"
 
 WEB_ROOT="/var/www"
-PHP_VERSIONS=( "5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0" "8.1" )
+PHP_VERSIONS=( "5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0" "8.1" "8.2" )
 
 # Install nginx
 sudo add-apt-repository ppa:nginx/stable -y
